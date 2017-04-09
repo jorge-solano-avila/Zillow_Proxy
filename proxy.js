3c033537adcca1aa64a963e6157ad1d6659f63d6
jorge52an <jorgesolavila@gmail.com>
2017-04-09 11:42:04 -0500

    Chore: add CORS headers

--- proxy.js
+++ proxy.js
@@ -2,3 +2,12 @@
 
 var express = require( "express" );
 var request = require( "request" );
+
+var app = express();
+
+app.use( function( request, response, next ) 
+{ 
+	response.header( "Access-Control-Allow-Origin", "*" ); 
+	response.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" ); 
+	next();
+} );
