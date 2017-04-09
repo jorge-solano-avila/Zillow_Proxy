e785b89a07002e595dd78cc6bb8e563f1b1f4843
jorge52an <jorgesolavila@gmail.com>
2017-04-09 12:03:16 -0500

    Chore: compose string query

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var express = require( "express" );
-var request = require( "request" );
+var requestAPI = require( "request" );
 
 var app = express();
 
@@ -11,3 +11,21 @@ app.use( function( request, response, next )
 	response.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" ); 
 	next();
 } );
+
+app.get( "/zillow-api", function( request, response )
+{
+	var requestZillow = request.query.url + "?";
+	for( var parameter in request.query )
+	{
+		if( parameter === "url" )
+			continue;
+		requestZillow += parameter + "=" + request.query[parameter] + "&";
+	}
+	requestZillow = requestZillow.substring( 0, requestZillow.length - 1 );
+	//requestAPI.get( { url:  } )
+	response.send( {} );
+} );
+
+var port = process.env.PORT || 3000;
+console.log( "Running in port " + port );
+app.listen( port );
